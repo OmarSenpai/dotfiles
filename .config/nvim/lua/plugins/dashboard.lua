1ea7915c1b1c9149e97dd15ca0d8bb1b97549a60
Omar Mohammed <omar80747326@gmail.com>
2026-01-07 09:32:45 +0200

    added a new nvim logo xD

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -6,7 +6,7 @@ return {
     },
 
     config = function()
-        local logo_file_path = vim.fn.stdpath("config") .. "/logos/nn_logo.txt"
+        local logo_file_path = vim.fn.stdpath("config") .. "/logos/solo_leveler.txt"
 
         -- Function to read the logo file
         local function read_logo(filepath)
@@ -26,18 +26,28 @@ return {
         -- Get the header lines from the file
         local custom_header = read_logo(logo_file_path)
 
+        local padding = {}
+        for i = 1, 20 do -- Adjust number of lines
+            table.insert(padding, "")
+        end
+
+        -- Combine padding with header
+        for _, line in ipairs(custom_header) do
+            table.insert(padding, line)
+        end
+        custom_header = padding
+
+        -- Add spacing between logo and menu
+        for i = 1, 3 do -- Change the number to add more/less spacing
+            table.insert(custom_header, "")
+        end
+
 
         require('dashboard').setup({
             -- critical: make theme outside config {}
             theme = 'doom',
             config = {
                 header = custom_header,
-
-                hide = {
-                    statusline = false,
-                    tabline = false,
-                    winbar = false,
-                },
 
                 center = {
                     { icon = "󰈞 ", desc = "Find File                    ", action = "Telescope find_files", key = "f" },
@@ -49,8 +59,6 @@ return {
                     { icon = "󱎥 ", desc = "Mason                        ", action = "Mason", key = "m" },
                     { icon = "󰐥 ", desc = "Quit                         ", action = "qa", key = "q" },
                 },
-
-                footer = {}
             },
         })
     end
